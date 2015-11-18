d945b4e79467f0816624f47e90f08c49033e098a
Chris Wallwork <chris@unrefgugees.org>
2015-11-18 12:21:09 -0500

    added prod key

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -1,7 +1,7 @@
 'user-strict'
   var gw = new Groundwork({
     'api_url': 'https://api.thegroundwork.com',
-    'oauth_client_id': 'pub-un-test.refugee-survey-int-30wNLFqA4VCph7fWteAfRartPvyFP7XEuvE.XSI21OEpd4MFgLOqRPcGSdOTVxohT1XU18eLBVflSM2WjIHigw'
+    'oauth_client_id': 'pub-un.refugeesurvey-int-TMo.3qerdLuxruWXl9xSk2_6Lg_TZnhg9b1roTHtmgN8.TQmBi6niAwOfLQ_xBuQBiftjYKXCKn_Od2_Uu7dNw'
   });
 
   var id = {};
